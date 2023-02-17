a0b126323b2ee9c1c3c035bdf00307487e853b49
SmileyJack <jacksmiley26@gmail.com>
2023-02-17 14:23:43 -0600

    downsized data_rcvd_callback; added agent struct

---
+++ agent.py
@@ -0,0 +1,6 @@
+class Agent:
+    def __init__(self, node_id, responses):
+        self.node_id = node_id
+        self.responses = []
+
+    
--- ground_control.py
+++ ground_control.py
@@ -29,27 +29,31 @@ get_nodes_on_network()
 
 
 def data_rcvd_callback(xbee_message):
-    global response_log
-    global receive_log
-    global send_log
-
     received = millis()
     address = xbee_message.remote_device.get_64bit_addr()
     node_id = xnet.get_device_by_64(address).get_node_id()
     data = xbee_message.data.decode("utf8")
+    calculate_and_record_message_and_response_time(data)
+
+    
+def calculate_and_record_message_and_response_time(data):
+    global response_log
+    global receive_log
+    global send_log
+
     split_id_and_message = data.split(",")
+
     print(data)
     print(split_id_and_message[1])
+
     response_time = received - send_log.get(int(split_id_and_message[1]))
 
     new_tuple = (data, response_time)
 
     if (node_id not in receive_log.keys()):
-        response_log[node_id] = new_tuple
+        response_log[node_id] = Agent(node_id, [new_tuple])
     else:
-        response_log.get(node_id).append(new_tuple)
-    
-    
+        response_log.get(node_id).responses.append(new_tuple)
     
 
 ground.add_data_received_callback(data_rcvd_callback)
@@ -57,6 +61,7 @@ ground.add_data_received_callback(data_rcvd_callback)
 def main():
     global send_log
     global response_log
+
     for i in range(10):
         time.sleep(0.5)
         ground.send_data_broadcast(str(i))
