c287eeab22b4cdacec8ff258953e435a16857726
SmileyJack <jacksmiley26@gmail.com>
2023-02-24 16:47:48 -0600

    ack now supports remote agent communication

--- agent.py
+++ agent.py
@@ -3,3 +3,6 @@ class Agent:
         self.node_id = node_id
         self.responses = responses
         self.average_response_time = 0
+        self.altitude = 0
+        self.lattitude = 0
+        self.longitude = 0
